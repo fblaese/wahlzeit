d97353646bd17bad89cb28edcfcc0810d42a4716
Fabian Bläse <fabian@blaese.de>
2018-11-04 17:45:57 +0100

    Additional EmailService test

--- src/test/java/org/wahlzeit/services/mailing/EmailServiceTest.java
+++ src/test/java/org/wahlzeit/services/mailing/EmailServiceTest.java
@@ -31,11 +31,13 @@ public class EmailServiceTest {
 
 	EmailService emailService = null;
 	EmailAddress validAddress = null;
+	EmailAddress bccAddress = null;
 
 	@Before
 	public void setup() throws Exception {
 		emailService = EmailServiceManager.getDefaultService();
 		validAddress = EmailAddress.getFromString("test@test.de");
+		bccAddress = EmailAddress.getFromString("bingo@example.com");
 	}
 
 	@Test
@@ -53,6 +55,15 @@ public class EmailServiceTest {
 	public void testSendValidEmail() {
 		try {
 			assertTrue(emailService.sendEmailIgnoreException(validAddress, validAddress, "hi", "test"));
+		} catch (Exception ex) {
+			Assert.fail("Silent mode does not allow exceptions");
+		}
+	}
+
+	@Test
+	public void testSendBccEmail() {
+		try {
+			assertTrue(emailService.sendEmailIgnoreException(validAddress, validAddress, bccAddress, "hi", "test"));
 		} catch (Exception ex) {
 			Assert.fail("Silent mode does not allow exceptions");
 		}
