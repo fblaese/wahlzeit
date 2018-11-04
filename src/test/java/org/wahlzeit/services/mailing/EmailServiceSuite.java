f91aae41f4945d9582da9269c4bf0eabf2a28fd2
Fabian Bläse <fabian@blaese.de>
2018-11-04 18:33:04 +0100

    Add test suite for EmailService

---
+++ src/test/java/org/wahlzeit/services/mailing/EmailServiceSuite.java
@@ -0,0 +1,12 @@
+package org.wahlzeit.services.mailing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	org.wahlzeit.services.mailing.EmailServiceTest.class
+})
+
+public class EmailServiceSuite {
+}
